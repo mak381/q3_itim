be5e50ab76d03bc26726bfaaf43bdf30fa1d56fd
root <root@localhost.localdomain>
2021-07-29 02:58:00 -0400

    initial commit

---
+++ add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+	void main(){
+		int a,b;
+		printf("Enter the first number\n");
+		scanf("%d",&a);
+		printf("Enter the second number\n");
+		scanf("%d",&b);				
+		printf("The sum of %d and %d is %d\n",a,b,a+b);
+}
